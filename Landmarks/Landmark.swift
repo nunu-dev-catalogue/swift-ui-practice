84b214279379a627c568f94a03c6af477c7b65ee
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-01-02 04:09:54 +0900

    Add Landmark, ModelData File

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,37 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by HyunWoo Lee on 2024/01/02.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Landmarks/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by HyunWoo Lee on 2024/01/02.
+//
+
+import Foundation
