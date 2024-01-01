77a84828402c0f3603a4824a7a38d98cd162755b
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-01-02 04:17:36 +0900

    Add LandmarkList

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by HyunWoo Lee on 2024/01/02.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks, id: \.id) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
