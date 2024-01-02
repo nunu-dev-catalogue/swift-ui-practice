b2cc94235951ab40698250854b53b669ce41743c
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-01-02 17:39:19 +0900

    Separate view package and add toggle in detail view

---
+++ Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by HyunWoo Lee on 2024/01/02.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
+        @Bindable var modelData = modelData
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
@@ -18,8 +25,12 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
 
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
@@ -42,5 +53,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
