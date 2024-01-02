b112af4eb18442129401670e00224ebc625fb49d
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-01-02 19:16:36 +0900

    Complete the category view

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -12,6 +12,10 @@ class ModelData {
     var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
     
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
     var categories: [String: [Landmark]] {
         Dictionary(
             grouping: landmarks,
--- Landmarks/Views/Category/CategoryHome.swift
+++ Landmarks/Views/Category/CategoryHome.swift
@@ -13,9 +13,16 @@ struct CategoryHome: View {
     var body: some View {
         NavigationSplitView {
             List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                    Text(key)
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
             }
             .navigationTitle("Featured")
         } detail: {
