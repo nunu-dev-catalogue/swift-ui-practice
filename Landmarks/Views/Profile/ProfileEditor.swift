57bd2fde8c449b40396ddaba1619e2392a4f91b4
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-01-02 20:01:16 +0900

    Define profile editor

---
+++ Landmarks/Views/Profile/ProfileEditor.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by HyunWoo Lee on 2024/01/02.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username")
+                Spacer()
+                TextField("UserName", text: $profile.username)
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.trailing)
+            }
+            
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable Notification")
+            }
+            
+            Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                ForEach(Profile.Season.allCases) { season in
+                    Text(season.rawValue).tag(season)
+                }
+            }
+            
+            DatePicker(selection: $profile.goalDate, in: dateRange, displayedComponents: .date) {
+                Text("Goal Date")
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileEditor(profile: .constant(.default))
+}
