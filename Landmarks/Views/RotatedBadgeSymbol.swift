36cbeb7c8f817505140769c22d6534625171f2a4
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-01-02 18:11:06 +0900

    Add BadgeSymbol and rotate version

---
+++ Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,21 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by HyunWoo Lee on 2024/01/02.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
